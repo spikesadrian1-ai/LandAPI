ffa7c964407714e5395548b2124c5e77574bd406
Adrian Spikes <spikesadrian1@gmail.com>
2026-05-07 18:54:45 -0500

    FIxed Error 26 SQL Server Error Cause: Somehow serve name was incorrect. Not sure why this happened.

--- OG_LandAPI/Program.cs
+++ OG_LandAPI/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Serialization;
 using OG_LandAPI.Models;
 
@@ -14,45 +13,38 @@ internal class Program
 
         // Add services to the container.
         builder.Services.AddControllers();
+
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
-        /// ENABLE C.O.R.S
-        /// 
+        // ENABLE C.O.R.S
         builder.Services.AddCors(c =>
         {
             c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
         });
 
 
-        /////
-        /// JSON SERIALIZER
-        ///                
+        // JSON SERIALIZER
         builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
         options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
             .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
             = new DefaultContractResolver());
-
-
-        ///
-        builder.Services.AddControllers();
-
+                        
         /// ADDED SWAGGER/OPEN API
-        //builder.Services.AddSwaggerGen(c =>
-        //{
-        //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
-        //});
+        ///builder.Services.AddSwaggerGen(c =>
+        ///{
+        ///    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
+        ///});
 
         //builder.Services.AddConnections();
 
-        ///   DATABASE CONNECTION
-        ///     ADD TO CONNECT TO SPECIFIC CLASSES USING EFCORE
+        // DATABASE CONNECTION: ADD TO CONNECT TO SPECIFIC CLASSES USING EFCORE
         builder.Services.AddDbContext<OGDatabaseSchemaV2Context>(options =>
         options.UseSqlServer("DevConnection"));
 
         var app = builder.Build();
 
-        ///ENABLE CORS
+        // ENABLE CORS
         app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 
@@ -60,14 +52,15 @@ internal class Program
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
+            //app.UseDeveloperExceptionPage();
 
             /// ADDED SWAGGER ENDPOINTS
             //app.UseSwagger();
             //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
         }
 
-        app.UseDefaultFiles();
-        app.UseStaticFiles();
+        //app.UseDefaultFiles();
+        //app.UseStaticFiles();
 
         app.UseHttpsRedirection();
 
